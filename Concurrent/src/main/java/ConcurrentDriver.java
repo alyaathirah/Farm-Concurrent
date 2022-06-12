a63d6f0f7dfc305e763b5b788d59455e9d6957d1
Letsplay-github <redizz501@gmail.com>
2022-06-12 11:37:35 +0800

    Cleanup comments in Sequential and Concurrent driver class

--- Concurrent/src/main/java/ConcurrentDriver.java
+++ Concurrent/src/main/java/ConcurrentDriver.java
@@ -1,25 +1,17 @@
 import DB.Database;
 import DB.Seeder;
 import DB.Table;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.lang3.time.StopWatch;
 
-
-/**
- * TODO
- * - create relationship of farms and its plants,fertilizers,pesticides
- */
 public class ConcurrentDriver {
     public static void main(String[] args){
-        System.out.println("Running concurrent program");
+        System.out.println("Running Concurrent Program");
         StopWatch watch = new StopWatch();
-        /**
-         * Create database
-         */
+        
+        //create database
         Database createDatabase = new Database();
         Table createTables = new Table();
         Seeder createSeeder = new Seeder();
@@ -27,10 +19,11 @@ public class ConcurrentDriver {
         createTables.tableManager();
         createSeeder.seederManager(101, 101, 101, 11, 101);
 
-        watch.start();  
-        int nFarmer = 100;
+        //start timer (to measure the speed of activity)
+        watch.start();
 
-        //Create activities for each users
+        //sreate activities for each farmers
+        int nFarmer = 100;
         Farm[] farms = new Farm[10];
         for(int i=0; i<10; i++){
             farms[i] = new Farm(i+1);
@@ -46,9 +39,7 @@ public class ConcurrentDriver {
         //start farmers' activities
         for(int i=0; i<nFarmer; i++){
             farmersT[i].start();
-//            farmers[i].run();
         }
-
 
         for (Thread thread : farmersT) {
             try {
@@ -59,8 +50,8 @@ public class ConcurrentDriver {
         }
         System.out.println("Thread joined");
         
-        //START of RUN INTERRUPTED ACTIVITY (turns)
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        //running n skipped activity (interrupted activity)
+        ExecutorService executorService = Executors.newFixedThreadPool(16);
         for (Farmer sfarmer : Farmer.skipFarmer) {
             executorService.execute(sfarmer);
         }
@@ -71,15 +62,16 @@ public class ConcurrentDriver {
             e.printStackTrace();
         }
         createSeeder.closeConn();
-        //END of RUN INTERRRUPTED ACTIVITY (turns)
+
+        //end timer
         watch.stop();
+
+        //simple report
         System.out.println("Activity excuted: "+Farmer.totalAct + " Interrupted: "+Farmer.totalInterruptedAct);
         System.out.println(watch.getTime()+"ms");
-        //Create activity
 
         //input log target
         InputLog inputlog = new InputLog();
         inputlog.inputlog();
-
     }
 }
--- Sequential/src/main/java/SequentialDriver.java
+++ Sequential/src/main/java/SequentialDriver.java
@@ -3,19 +3,12 @@ import DB.Seeder;
 import DB.Table;
 import org.apache.commons.lang3.time.StopWatch;
 
-
-/**
- * TODO
- * - create relationship of farms and its plants,fertilizers,pesticides
- */
 public class SequentialDriver {
-    public static void main(String[] args){
-        System.out.println("Running sequential program");
+    public static void main(String[] args) {
+        System.out.println("Running Sequential Program");
         StopWatch watch = new StopWatch();
-        watch.start();
-        /**
-         * Create database
-         */
+        
+        //Create database
         Database createDatabase = new Database();
         Table createTables = new Table();
         Seeder createSeeder = new Seeder();
@@ -23,29 +16,33 @@ public class SequentialDriver {
         createTables.tableManager();
         createSeeder.seederManager();
 
+        //Start Timer (to measure the speed of activity)
+        watch.start();
 
-        //Create activities for each users
+        // Create activities for each farmers
+        int nFarmer = 100;
         Farm[] farms = new Farm[10];
-        for(int i=0; i<10; i++){
-            farms[i] = new Farm(i+1);
+        for (int i = 0; i < 10; i++) {
+            farms[i] = new Farm(i + 1);
         }
-        Farmer[] farmers = new Farmer[100];
-//        Thread[] farmersT = new Thread[100];
-        for(int i=0; i<100; i++){
-            farmers[i] = new Farmer(i+1,farms);
-//            farmersT[i] = new Thread(farmers[i]);
+        Farmer[] farmers = new Farmer[nFarmer];
+        for (int i = 0; i < nFarmer; i++) {
+            farmers[i] = new Farmer(i + 1, farms);
         }
 
-        //start farmers' activities
-        for(int i=0; i<100; i++){
+        // start farmers' activities
+        for (int i = 0; i < nFarmer; i++) {
             farmers[i].run();
         }
-        watch.stop();
-        System.out.println(watch.getTime()+"ms");
 
-        //input log target
+        //end timer
+        watch.stop();
+
+        //simple report
+        System.out.println(watch.getTime() + "ms");
+
+        // input log target
         InputLog inputlog = new InputLog();
         inputlog.inputlog();
-
     }
 }
